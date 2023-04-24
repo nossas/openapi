9804fd66627e5eacef51ebe50447cf68eb368613
Igor Santos <igr.exe@gmail.com>
2023-04-24 13:30:09 -0300

    feat(ci): add stack to deploy portainer

--- api/project/settings/development.py
+++ api/project/settings/development.py
@@ -40,7 +40,10 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = [env.list("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+if type(ALLOWED_HOSTS) is not list:
+    ALLOWED_HOSTS = [ALLOWED_HOSTS]
 
 
 # Application definition
